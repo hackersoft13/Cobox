a6c92d51fa5a8f9f129b14f978081e7335f7c27b
Administrator <admin@example.com>
2019-11-30 15:03:43 +0100

    g tout cassé

--- CoCaptor_src/WiFiClient/WiFiClient.ino
+++ CoCaptor_src/WiFiClient/WiFiClient.ino
@@ -8,14 +8,14 @@
 #include <ESP8266WiFiMulti.h>
 
 #ifndef STASSID
-#define STASSID "MartinRouterKing"
-#define STAPSK  "pass"
+#define STASSID "Ominux"
+#define STAPSK  "vr5d7tEW"
 #endif
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
-const char* host = "192.168.2.20";
+const char* host = "10.42.0.1";
 const uint16_t port = 4444;
 ESP8266WiFiMulti WiFiMulti;
 
@@ -61,7 +61,7 @@ void loop() {
   }
 
   // This will send the request to the server
-  client.println("1;23;56\r");
+  client.println("2;23;56\r");
   delay(5000);
   //read back one line from server
   Serial.println("receiving from remote server");
--- Cobox_src/V2/TCP_server.c
+++ Cobox_src/V2/TCP_server.c
@@ -7,12 +7,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
-#include<sys/wait.h> 
+#include <sys/wait.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
 #include "TCPserver.h"
 #define PORT 4444
 
-int main(){
-
+int main()
+{
+	// Création de la FIFO
+	//char * myfifo = "/home/paul/Documents/Cobox/Cobox/Cobox_src/V2/fifo";
+	//mkfifo(myfifo, 0766);
+	// Fin création FIFO
+	//char *fifosend = "/home/paul/Documents/Cobox/Cobox/Cobox_src/V2/fifo";
+	int fd;
+	mkfifo("fifo", 0666);
+	//----------Début socket
 	int sockfd, ret, i;
 	struct sockaddr_in serverAddr;
 	int newSocket;
@@ -20,11 +32,13 @@ int main(){
 	socklen_t addr_size;
 	char buffer[1024];
 	pid_t childpid;
-	mesure *m=NULL;
-	m = (mesure*)malloc(sizeof(mesure)); 
-	i=0;
+	//---------Fin Socket
+	mesure *m = NULL;
+	m = (mesure *)malloc(sizeof(mesure));
+	i = 0;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
+	if (sockfd < 0)
+	{
 		printf("[-]Error in connection.\n");
 		exit(1);
 	}
@@ -35,69 +49,103 @@ int main(){
 	serverAddr.sin_port = htons(PORT);
 	serverAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
-	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
+	ret = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	if (ret < 0)
+	{
 		printf("[-]Error in binding.\n");
 		exit(1);
 	}
 	printf("[+]Bind to port %d\n", 4444);
 
-	if(listen(sockfd, 10) == 0){
+	if (listen(sockfd, 10) == 0)
+	{
 		printf("[+]Listening....\n");
-	}else{
+	}
+	else
+	{
 		printf("[-]Error in binding.\n");
 	}
 
-
-	while(1){
-		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
-		if(newSocket < 0){
+	while (1)
+	{
+		newSocket = accept(sockfd, (struct sockaddr *)&newAddr, &addr_size);
+		if (newSocket < 0)
+		{
 			exit(1);
 		}
 		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
 		printf("Mesure n° %d\n", i);
 		childpid = fork();
-		if(childpid == 0){
+		if (childpid == 0)
+		{
 			close(sockfd);
 
-			while(1){
+			while (1)
+			{
 				recv(newSocket, buffer, 1024, 0);
-				if (strlen(buffer) > 3){
-					if(strcmp(buffer, ":exit") == 0){
+				if (strlen(buffer) > 3)
+				{
+
+					if (strcmp(buffer, ":exit") == 0)
+					{
 						printf("Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
 						exit(0);
 						break;
-					}else{
-						
-						
-						(m+i)->id=atoi(strtok(buffer, ";"));
-						(m+i)->temp=atoi(strtok(NULL,";"));
-						(m+i)->hum=atoi(strtok(NULL,";"));
-						m = realloc(m,(sizeof(m))+sizeof(mesure));
+					}
+					else
+					{
+						printf("Ouverture FIFO\n");
+
+						fd=open("fifo", O_RDWR);
+
+						//close(fd);
+						char str2[80];
+						(m + i)->id = atoi(strtok(buffer, ";"));
+						(m + i)->temp = atoi(strtok(NULL, ";"));
+						(m + i)->hum = atoi(strtok(NULL, ";"));
+						m = realloc(m, (sizeof(m)) + sizeof(mesure));
 						printf("Device ID : %d\nTemperature : %d\nHumidity : %d\n", m[i].id, m[i].temp, m[i].hum);
+						strcpy(str2, "1;23;54\n");
+						write(fd, str2, sizeof(str2));
+						close(fd);
 						send(newSocket, "1;1;1\r", 10, 0);
+
 						//sleep(4);
 						bzero(buffer, sizeof(buffer));
 						i++;
 					}
 				}
-				
 			}
 			//
 			//exit(0);
-		} else 
+		}
+		else
 		{
 			wait(NULL);
+			char input;
+			int n;
+			//----Récupération de la données via FIFO
+			//fd1 = open(myfifo,O_RDONLY);
+			//read(fd1, str1, strlen(buffer));
+
+			// Print the read string and close
+			//printf("User1: %s\n", str1);
+			//close(fd1);
+			char str1[80];
+			
+			fd = open("fifo", O_RDONLY);
+			printf("Ouverture de reception\n");
+			while((n=read(fd, &input, 1)) > 0)
+			{
+				printf(" char : %c",input);
+			}
+			close(fd);
 		}
-		
+
 		i++;
-
-		
-
 	}
-	
-	close(newSocket);
 
+	close(newSocket);
 
 	return 0;
 }
